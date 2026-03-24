08920c7b5a39bcabe0abc2b4e733e941633ef9dc
KNikhith <nikhithkotluri@gmail.com>
2026-03-24 21:28:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let count=0;
+let counter=document.getElementById("counter");
+let incrementBtn=document.getElementById("incrementBtn");
+incrementBtn.addEventListener("click",()=>{
+	alert(count);
+	count++;
+	counter.textContent=count;
+})
